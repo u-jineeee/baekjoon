4eeba66680913c61faf0f4be4e50b04f0158c915
U Jineeee <80236952+u-jineeee@users.noreply.github.com>
2022-09-20 22:16:01 +0900

    Add files via upload

---
+++ Silver5/25501_재귀의 귀재.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main{
+    public static int cnt = 0;
+
+    public static int recursion(String s, int l, int r){
+        cnt++;
+        if(l >= r) return 1;
+        else if(s.charAt(l) != s.charAt(r)) return 0;
+        else return recursion(s, l+1, r-1);
+    }
+    public static int isPalindrome(String s){
+        return recursion(s, 0, s.length()-1);
+    }
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < n; i++) {
+            cnt = 0;
+            String s = br.readLine();
+            sb.append(isPalindrome(s)).append(" ").append(cnt).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
