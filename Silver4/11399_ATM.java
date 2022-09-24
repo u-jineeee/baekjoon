60479aee0fb7d2c0f50815b8f431b5d2398aec61
U Jineeee <80236952+u-jineeee@users.noreply.github.com>
2022-09-24 23:36:25 +0900

    Add files via upload

---
+++ Silver4/11399_ATM.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0; i < n; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(arr);
+
+        int sum = 0;
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < i + 1; j++)
+                sum += arr[j];
+
+        System.out.println(sum);
+    }
+}
